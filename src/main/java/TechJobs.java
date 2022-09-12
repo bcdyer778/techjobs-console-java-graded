9eb4bfd277cd8c1a2dd46e3b9dec888e15a32866
bcdyer778 <99931077+bcdyer778@users.noreply.github.com>
2022-09-12 12:57:27 -0500

    should be working?

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -97,9 +97,11 @@ public class TechJobs {
             if (in.hasNextInt()) {
                 choiceIdx = in.nextInt();
                 in.nextLine();
+
             } else {
                 String line = in.nextLine();
                 boolean shouldQuit = line.equals("x");
+
                 if (shouldQuit) {
                     return null;
                 }
@@ -108,6 +110,7 @@ public class TechJobs {
             // Validate user's input
             if (choiceIdx < 0 || choiceIdx >= choiceKeys.length) {
                 System.out.println("Invalid choice. Try again.");
+
             } else {
                 validChoice = true;
             }
@@ -120,6 +123,21 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.isEmpty()){
+            System.out.println("No Results");
+
+        } else {
+
+            for (int i = 0; i < someJobs.size(); i++){
+                System.out.println("\n"+"*****");
+
+                for (String key:someJobs.get(i).keySet()){
+                    System.out.println(key+": "+someJobs.get(i).get(key));
+                }
+
+                System.out.println("*****");
+            }
+        }
+
     }
 }
